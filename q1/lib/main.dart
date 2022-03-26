789537693ecbf670e013cf4fd2a690402f129114
Huraira <muhammadhuraira4@gmail.com>
2022-03-26 11:08:19 +0500

    Lab3 second commit

--- q1/lib/main.dart
+++ q1/lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.brown,
-      title: Text("Mobile App Dev lab"),
-    ),
-    backgroundColor: Colors.blueGrey,
-    body: Center(child: Text('You have pressed the button 0 times')),
-    floatingActionButton: FloatingActionButton(
-      onPressed: null,
-      child: Icon(Icons.add),
-      tooltip: "This button is not functional",
-    ),
-  )));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+        title: Text("Mobile App Dev lab"),
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Center(child: Text('You have pressed the button 0 times')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
+        tooltip: "This button is not functional",
+      ),
+    ));
+  }
 }
